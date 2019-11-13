41b5dbce6f2b3b6bf137d2089ffedc0aec97f824
duc <dangduc181199@gmail.com>
2019-11-13 15:16:26 +0700

    labwork 4

--- WeatherActivity2/USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ WeatherActivity2/USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -1,18 +1,35 @@
 package vn.edu.usth.usthweather;
-import androidx.fragment.app.Fragment;
+
 import android.content.Context;
+import android.app.Fragment;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
 import android.view.View;
+import android.view.ViewGroup;
+
 public class ForecastFragment extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+//
+      //  View view = new View(getContext());
 
-        View view = new View(getContext());
+      //  view.setBackgroundColor(0x0000FF00);
+      //  return view;
 
-        view.setBackgroundColor(0x0000FF00);
-        return view;
+
+View view = inflater.inflate(R.layout.forecastfragment, container,false);
+      TextView textview = new TextView(getContext());
+      textview.setText("wednesday");
+      ImageView imageview = new ImageView(getContext());
+      imageview.setMaxHeight(250);
+      imageview.setMaxWidth(250);
+      imageview.setBackgroundResource(R.drawable.Sunny);
+           return view;
+
     }
 }
